a58bcec97ecf8c5d31dafa3c87c978d260d22c8b
Oyham <voyhamburu@gmail.com>
2024-06-02 22:03:18 -0300

    add about & footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,12 +39,49 @@ const Footer = () => {
               </label>
             </div>
           </div>
-          <span>Ver información básica sobre protección de datos <i className="ri-arrow-down-s-line"></i></span>
+          <span>
+            Ver información básica sobre protección de datos{" "}
+            <i className="ri-arrow-down-s-line"></i>
+          </span>
         </div>
+      </section>
+
+      <section className="section container about__container">
+        <article className="about__ul-container">
+          <h3>SOBRE NOSOTROS</h3>
+          <div className="about__ul">
+            <ul>
+              <li>Avoris</li>
+              <li>Condiciones Generales</li>
+              <li>Protección de detos</li>
+              <li>Política de cookies</li>
+              <li>Política de privacidad</li>
+              <li>Póliza de insolvencia</li>
+              <li>Blog</li>
+              <li>Bases Legales Campaña Caribe</li>
+              <li>Bases Legales Campaña Venta Anticipada 2024</li>
+              <li>Bases Legales Campaña Circuitos por Europa hasta 15%</li>
+              <li>Bases Legales Campaña MGM</li>
+              <li>Trabaja con nosotros</li>
+              <li>Canal Denuncias</li>
+            </ul>
+          </div>
+        </article>
+
+        <article className="about__follow">
+          <span>¡Síguenos!</span>
+          <label htmlFor="">Llámanos al 932 08 70 00</label>
+          <label htmlFor="">De Lunes a Viernes de 09:00 a 19:00 h</label>
+          <label htmlFor="">Email de atención al viajero: </label>
+          <bold className="mail">viatges.atv-fono@racctravel.com</bold>
+        </article>
       </section>
 
       <picture>
         <img src={logoFooter} alt="img__logo-footer" />
+        <p>
+        © Sextante Viajes S.L. AVBAL/780 con C.I.F. B57797813
+        </p>
       </picture>
     </footer>
   );
